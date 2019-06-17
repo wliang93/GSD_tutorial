02b28c586a699b3a47f4a4b3001a03e88a997eb8
wliang93 <will.liang@live.com>
2019-06-16 19:28:11 -0700

    hi

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,1 @@
-print ("wtf just happened")
-print("hji")
-print("commit")
-<<<<<<< HEAD
-print("commit2")
-print("dragandrop")
-=======
-print("demotest")
->>>>>>> 9fa0479ad108b786cf9421d54bb9cf0cc7ce97d2
+print("helloworld")
